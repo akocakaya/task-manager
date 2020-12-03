0f56af8162359b8333bfd99f5ba1c4056d96724b
akocakaya <abdullahkocakaya4@gmail.com>
2020-12-03 23:22:53 +0300

    feat: returning lean documents mongoose

--- src/modules/task/task.model.ts
+++ src/modules/task/task.model.ts
@@ -5,12 +5,23 @@ export interface ITask extends Document {
   author: string;
 }
 
+// transform for sending as json
+function omitPrivate(_doc: any, obj: any) {
+  delete obj.__v;
+  return obj;
+}
+
 const TaskSchema: Schema = new Schema(
   {
     text: { type: String, required: true },
     author: { type: String, required: true },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: omitPrivate,
+    },
+  },
 );
 
 export default model<ITask>('Task', TaskSchema);
