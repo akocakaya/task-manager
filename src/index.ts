23497a18fc77ef5f5a69a9161815017bb2d31688
akocakaya <abdullahkocakaya4@gmail.com>
2020-12-05 17:37:35 +0300

    refactor: db name

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const bootServer = async () => {
 
   try {
     await mongoose.connect(
-      'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false/task-db',
+      'mongodb://localhost:27017/task-db',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
