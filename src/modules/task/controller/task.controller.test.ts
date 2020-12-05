10fd4d9530c723682675ba81c7947956a8bfffb9
akocakaya <abdullahkocakaya4@gmail.com>
2020-12-05 19:04:59 +0300

    feat: first test for createTask function

--- src/modules/task/controller/task.controller.test.ts
+++ src/modules/task/controller/task.controller.test.ts
@@ -1,16 +1,26 @@
-import express from 'express';
-const router = express.Router();
+import { Request, Response } from 'express';
 import taskModel from '../entity/task.model';
+import { createTask } from './task.controller';
+
 jest.mock('../entity/task.model');
+jest.mock('express');
 
-describe('Modules > Task > Repository > Task Repository', () => {
-  it('should call task model with texst and author', () => {
-    // Given
-    const req = { body: { text: 'testText', author: 'testAuthor' } };
-    const res = { status: { send: jest.fn() } };
+describe('Modules > Task > Controller > Task Controller', () => {
+  describe('Create Task', () => {
+    it('should call task model with text and author', () => {
+      // Given
+      const req = { body: { text: 'testText', author: 'testAuthor' } } as Request;
+      const res = ({
+        status: jest.fn().mockImplementationOnce(() => ({
+          send: jest.fn().mockImplementationOnce(() => ({})),
+        })),
+      } as unknown) as Response;
 
-    // When
+      // When
+      createTask(req, res);
 
-    // Then
+      // Then
+      expect(taskModel).toBeCalledWith(req.body);
+    });
   });
 });
