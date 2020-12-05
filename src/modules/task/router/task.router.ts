cbb7b40ec58e0e505ffd59884a1a82d29f87f8b8
akocakaya <abdullahkocakaya4@gmail.com>
2020-12-05 22:10:59 +0300

    fix: router get task path from route param to query param

--- src/modules/task/router/task.router.ts
+++ src/modules/task/router/task.router.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 import { createTask, getTask, getTaskList } from '../controller/task.controller';
 
 router.post('/', createTask);
-router.get('/:id', getTask);
+router.get('/', getTask);
 router.get('/getTaskList', getTaskList);
 
 export default router;
